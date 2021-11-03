edfd30e35b27da769abaa057962b0b5579c67c5d
Davit <4066753+davitjnz@users.noreply.github.com>
2021-11-04 00:19:52 +0400

    Add files via upload

---
+++ data/actions.js
@@ -0,0 +1,102 @@
+const actions = [
+  {
+    "curated": "True",
+    "tags": [],
+    "platform": "youtube",
+    "use_batch": "False",
+    "search_terms_tags": "['*']",
+    "data_source_tag": "['mini-set']",
+    "last_collection_date": "2021-10-18 22:00:00",
+    "id": "48f4d8b3-396a-4a85-a892-f5c4902bed68"
+  },
+  {
+    "curated": "True",
+    "tags": [],
+    "platform": "facebook",
+    "use_batch": "True",
+    "search_terms_tags": "['*']",
+    "data_source_tag": "['mini-set']",
+    "last_collection_date": "2021-10-18 01:00:00",
+    "id": "2d37fdcf-0b1b-43f8-9f88-d0f5e2b194dc"
+  },
+  {
+    "curated": "True",
+    "tags": [],
+    "platform": "tv_georgia",
+    "use_batch": "False",
+    "search_terms_tags": "['*']",
+    "data_source_tag": "",
+    "last_collection_date": "2021-10-15 00:00:00",
+    "id": "44306cbb-7020-45f0-b446-53e23dd6030b"
+  },
+  {
+    "curated": "True",
+    "tags": [],
+    "platform": "facebook",
+    "use_batch": "True",
+    "search_terms_tags": "['*']",
+    "data_source_tag": "",
+    "last_collection_date": "2021-10-15 00:00:00",
+    "id": "1e6434f2-e01d-48a8-8b90-c8723029fcaa"
+  },
+  {
+    "curated": "True",
+    "tags": [],
+    "platform": "facebook",
+    "use_batch": "True",
+    "search_terms_tags": "['*']",
+    "data_source_tag": "",
+    "last_collection_date": "2021-10-15 00:00:00",
+    "id": "060d5a83-3883-475d-8317-c9d34410a089"
+  },
+  {
+    "curated": "False",
+    "tags": [],
+    "platform": "facebook",
+    "use_batch": "True",
+    "search_terms_tags": "['*']",
+    "data_source_tag": "",
+    "last_collection_date": "2021-10-15 00:00:00",
+    "id": "1765224f-7671-40a5-8047-97146ce8d1d4"
+  },
+  {
+    "curated": "False",
+    "tags": [],
+    "platform": "tiktok",
+    "use_batch": "True",
+    "search_terms_tags": "['*']",
+    "data_source_tag": "",
+    "last_collection_date": "2021-10-15 00:00:00",
+    "id": "e258335a-d67c-4c8f-b80f-1085be0968f5"
+  },
+  {
+    "curated": "False",
+    "tags": [],
+    "platform": "tv_georgia",
+    "use_batch": "True",
+    "search_terms_tags": "['*']",
+    "data_source_tag": "",
+    "last_collection_date": "2021-10-15 00:00:00",
+    "id": "38f25209-12d2-466a-ae9c-1313db1437b5"
+  },
+  {
+    "curated": "True",
+    "tags": [],
+    "platform": "youtube",
+    "use_batch": "True",
+    "search_terms_tags": "",
+    "data_source_tag": "",
+    "last_collection_date": "2021-10-15 00:00:00",
+    "id": "895935e9-1253-45ea-8674-e88bb9956186"
+  },
+  {
+    "curated": "False",
+    "tags": "['activated']",
+    "platform": "twitter",
+    "use_batch": "True",
+    "search_terms_tags": "",
+    "data_source_tag": "",
+    "last_collection_date": "2021-10-15 00:00:00",
+    "id": "aa4d65c9-e2fc-4f71-a885-06be4cc5857f"
+  }
+]
